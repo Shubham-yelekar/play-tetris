5cf0c79de1ce18c07162d1200392913eadf0d8d7
Shubham Yelekar <shubhamyelekar0@gmail.com>
2023-03-28 20:25:29 +0530

    first

---
+++ app.js
@@ -0,0 +1,58 @@
+
+let canvas = document.querySelector('#tetris');
+let scoreboard = document.querySelector('h2')
+let ctx = canvas.getContext('2d');  // 2d Drawing Board
+ctx.scale(30,30)
+
+
+const SHAPES = [
+    [
+        [0,1,0,0],
+        [0,1,0,0],
+        [0,1,0,0],
+        [0,1,0,0]
+    ],
+    [
+        [0,1,0],  
+        [0,1,0],  
+        [1,1,0]   
+    ],
+    [
+        [0,1,0],
+        [0,1,0],
+        [0,1,1]
+    ],
+    [
+        [1,1,0],
+        [0,1,1],
+        [0,0,0]
+    ],
+    [
+        [0,1,1],
+        [1,1,0],
+        [0,0,0]
+    ],
+    [
+        [1,1,1],
+        [0,1,0],
+        [0,0,0]
+    ],
+    [
+        [1,1],
+        [1,1],
+    ]
+]
+
+const COLORS = [
+    "#fff",
+    "#9b5fe0",
+    "#16a4d8",
+    "#60dbe8",
+    "#8bd346",
+    "#efdf48",
+    "#f9a52c",
+    "#d64e12"
+]
+
+const ROWS = 20;
+const COLS = 10;
